2364574a12129afb29100185d376f7d60dc3bc61
Ch-Amrutha-SN <105914357+Ch-Amrutha-SN@users.noreply.github.com>
2022-05-22 13:25:02 +0530

    Create app.js

---
+++ Calc_JS/app.js
@@ -0,0 +1,39 @@
+num1=document.getElementById("number1");
+num2=document.getElementById("number2");
+
+add=document.getElementById("Add");
+btn=function(){
+      ans=Number(num1.value)+Number(num2.value);
+      document.getElementById("result").value=ans;
+}
+add.addEventListener("click",btn);
+
+// Object.onclick=function Add(){
+//       ans=Number(num1.value)+Number(num2.value);
+//        document.getElementById("result").value=ans;
+// };
+// Object.addEventListener("click",Add);
+
+sub=document.getElementById("Sub");
+btn=function(){
+      ans=Number(num1.value)-Number(num2.value);
+
+      document.getElementById("result").value=ans;
+}
+sub.addEventListener("click",btn);
+
+mul=document.getElementById("Mul");
+btn=function(){
+      ans=Number(num1.value)*Number(num2.value);
+
+      document.getElementById("result").value=ans;
+}
+mul.addEventListener("click",btn);
+
+div=document.getElementById("Div");
+btn=function(){
+      ans=Number(num1.value)/Number(num2.value);
+
+      document.getElementById("result").value=ans;
+}
+div.addEventListener("click",btn);
